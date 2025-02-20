f35f94a05d08f5d6fc0156335379d5652c046d49
TL47 <tlopdel0611@g.educaand.es>
2025-02-20 10:07:56 +0100

    add A5

---
+++ A5.py
@@ -0,0 +1,9 @@
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(n-1-i):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+    return lista
+
+print(bubble_sort([8,6,1,4,200,7]))
